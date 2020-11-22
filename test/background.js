aac8d47252b643e79efc54afe6f430b4969d7597
Leochens <18332518328@163.com>
2020-11-22 16:22:08 +0800

    修改

--- test/background.js
+++ test/background.js
@@ -1,13 +1,1 @@
 // background.js
-chrome.runtime.onInstalled.addListener(function(){
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
-		chrome.declarativeContent.onPageChanged.addRules([
-			{
-				conditions: [
-					new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlContains: 'mp.weixin.qq.com'}})
-				],
-				actions: [new chrome.declarativeContent.ShowPageAction()]
-			}
-		]);
-	});
-});
