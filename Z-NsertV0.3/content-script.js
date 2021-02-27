f472241bb7e9e8bd6f112c3857c847a85247504f
Leochens <18332518328@163.com>
2021-02-27 11:24:57 +0800

    更改提示方式

--- Z-NsertV0.3/content-script.js
+++ Z-NsertV0.3/content-script.js
@@ -1,125 +1,146 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-	console.log('我被执行了！');
+  console.log('我被执行了！');
 });
 
 
-function getBody() {
-	console.log(document)
-	const editor = document.getElementById('ueditor_0');
-	const body = editor.contentWindow.document.getElementsByTagName('body')[0];
-	return body;
+function getBody () {
+  console.log(document)
+  const editor = document.getElementById('ueditor_0');
+  const body = editor.contentWindow.document.getElementsByTagName('body')[0];
+  return body;
 }
-
+var tipCount = 0;
+function alertMsg (msg, type) {
+  msg = msg || '';
+  var ele = document.createElement('div');
+  ele.className = 'chrome-plugin-simple-tip slideInLeft';
+  if (type === 'success') {
+    ele.className = 'chrome-plugin-simple-tip success slideInLeft';
+  }
+  ele.style.top = tipCount * 70 + 20 + 'px';
+  ele.innerHTML = `<div>${msg}</div>`;
+  document.body.appendChild(ele);
+  ele.classList.add('animated');
+  tipCount++;
+  setTimeout(() => {
+    ele.style.top = '-100px';
+    setTimeout(() => {
+      ele.remove();
+      tipCount--;
+    }, 400);
+  }, 3000);
+}
 // 接收来自后台的消息
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	// console.log('收到来自 ' + (sender.tab ? "content-script(" + sender.tab.url + ")" : "popup或者background") + ' 的消息：', request);
-	switch (request.cmd) {
-		case 'get_audio_upload': {
-			const body = getBody();
-			// 获取到的音频节点信息
-			const audios = body.getElementsByClassName("js_editor_audio")
-			console.log(audios);
-			if (!audios.length) return alert("未发现用户上传音乐!");
+  // console.log('收到来自 ' + (sender.tab ? "content-script(" + sender.tab.url + ")" : "popup或者background") + ' 的消息：', request);
+  switch (request.cmd) {
+    case 'get_audio_upload': {
+      const body = getBody();
+      // 获取到的音频节点信息
+      const audios = body.getElementsByClassName("js_editor_audio")
+      console.log(audios);
+      if (!audios.length) return alertMsg('未发现用户上传音乐')
 
-			const _audios = [];
-			for (let i = 0; i < audios.length; i++) {
-				const item = audios[i]
 
-				if (!item) return alert("未发现用户上传音乐!");
-				const code = item.getAttribute('voice_encode_fileid');
-				const name = item.getAttribute('name');
-				console.log(name, code);
-				_audios.push({ name, code });
-			}
-			console.log(_audios);
-			sendResponse(JSON.stringify(_audios));
-			break;
-		}
-		case 'get_audio_qqmusic': {
-			const body = getBody();
-			// 获取到的音频节点信息
-			const audios = body.getElementsByClassName("wx-edui-media-wrp")
-			console.log(audios);
-			if (!audios.length) return alert("未发现QQ音乐!");
+      const _audios = [];
+      for (let i = 0; i < audios.length; i++) {
+        const item = audios[i]
 
-			const _audios = [];
-			for (let i = 0; i < audios.length; i++) {
-				const item = audios[i]
-				const code = item.getElementsByClassName('js_editor_qqmusic')[0];
-				if (!code) return alert("未发现QQ音乐!");
-				const name = code.getAttribute('music_name');
-				_audios.push({ name, code: item.innerHTML });
-			}
-			console.log(_audios);
-			sendResponse(JSON.stringify(_audios));
-			break;
-		}
-		case 'get_video_upload': {
-			const body = getBody();
+        if (!item) return alertMsg("未发现用户上传音乐!");
+        const code = item.getAttribute('voice_encode_fileid');
+        const name = item.getAttribute('name');
+        console.log(name, code);
+        _audios.push({ name, code });
+      }
+      console.log(_audios);
+      sendResponse(JSON.stringify(_audios));
+      break;
+    }
+    case 'get_audio_qqmusic': {
+      const body = getBody();
+      // 获取到的音频节点信息
+      const audios = body.getElementsByClassName("wx-edui-media-wrp")
+      console.log(audios);
+      if (!audios.length) return alertMsg("未发现QQ音乐!");
 
-			const videoFrames = body.getElementsByClassName("video_iframe");
-			if (!videoFrames.length) return alert("未发现用户上传视频!");
+      const _audios = [];
+      for (let i = 0; i < audios.length; i++) {
+        const item = audios[i]
+        const code = item.getElementsByClassName('js_editor_qqmusic')[0];
+        if (!code) return alertMsg("未发现QQ音乐!");
+        const name = code.getAttribute('music_name');
+        _audios.push({ name, code: item.innerHTML });
+      }
+      console.log(_audios);
+      sendResponse(JSON.stringify(_audios));
+      break;
+    }
+    case 'get_video_upload': {
+      const body = getBody();
 
-			const _videos = [];
-			for (let i = 0; i < videoFrames.length; i++) {
-				const item = videoFrames[i];
-				if (!item) return alert("未发现用户上传视频!");
-				const code = item.getAttribute('data-mpvid');
-				const cover = item.getAttribute('data-cover');
-				_videos.push({ cover, code });
-			}
-			console.log(_videos);
-			sendResponse(JSON.stringify(_videos));
-			break;
-		}
-		case 'get_mimi_id': {
-			const body = getBody();
-			// 获取到的音频节点信息
-			const minis = body.getElementsByClassName("weapp_text_link");
-			if (!minis.length) return alert("未发现文字小程序!");
+      const videoFrames = body.getElementsByClassName("video_iframe");
+      if (!videoFrames.length) return alertMsg("未发现用户上传视频!");
 
-			console.log(minis);
-			const _minis = [];
-			for (let i = 0; i < minis.length; i++) {
-				const item = minis[i];
-				console.log(item);
-				if (!item) return alert("未发现文字小程序!!");
-				const appid = item.getAttribute('data-miniprogram-appid');
-				const path = item.getAttribute('data-miniprogram-path');
-				const name = item.getAttribute("data-miniprogram-nickname");
-				_minis.push({ appid, path, name });
-			}
-			console.log(_minis);
-			sendResponse(JSON.stringify(_minis));
-			break;
-		}
+      const _videos = [];
+      for (let i = 0; i < videoFrames.length; i++) {
+        const item = videoFrames[i];
+        if (!item) return alertMsg("未发现用户上传视频!");
+        const code = item.getAttribute('data-mpvid');
+        const cover = item.getAttribute('data-cover');
+        _videos.push({ cover, code });
+      }
+      console.log(_videos);
+      sendResponse(JSON.stringify(_videos));
+      break;
+    }
+    case 'get_mimi_id': {
+      const body = getBody();
+      // 获取到的音频节点信息
+      const minis = body.getElementsByClassName("weapp_text_link");
+      if (!minis.length) return alertMsg("未发现文字小程序!");
 
-		case 'import_code': {
-			const body = getBody();
-			// 获取到的音频节点信息
-			const code = request.code;
-			console.log(code);
+      console.log(minis);
+      const _minis = [];
+      for (let i = 0; i < minis.length; i++) {
+        const item = minis[i];
+        console.log(item);
+        if (!item) return alertMsg("未发现文字小程序!");
+        const appid = item.getAttribute('data-miniprogram-appid');
+        const path = item.getAttribute('data-miniprogram-path');
+        const name = item.getAttribute("data-miniprogram-nickname");
+        _minis.push({ appid, path, name });
+      }
+      console.log(_minis);
+      sendResponse(JSON.stringify(_minis));
+      break;
+    }
 
-			body.innerHTML = code;
+    case 'import_code': {
+      const body = getBody();
+      // 获取到的音频节点信息
+      const code = request.code;
+      console.log(code);
 
-			sendResponse(true);
+      body.innerHTML = code;
 
-			break;
-		}
+      sendResponse(true);
+      alertMsg("操作成功!", 'success');
+      break;
+    }
 
-		default:
-			{
-				// tip(JSON.stringify(request));
-				sendResponse('我收到你的消息了：' + JSON.stringify(request));
-			}
-	}
+    default:
+      {
+        // tip(JSON.stringify(request));
+        sendResponse('我收到你的消息了：' + JSON.stringify(request));
+      }
+  }
 
 });
 
 // 主动发送消息给后台
 // 要演示此功能，请打开控制台主动执行sendMessageToBackground()
-function sendMessageToBackground(message) {
-	chrome.runtime.sendMessage({ greeting: message || '你好，我是content-script呀，我主动发消息给后台！' }, function (response) {
-		tip('收到来自后台的回复：' + response);
-	});
+function sendMessageToBackground (message) {
+  chrome.runtime.sendMessage({ greeting: message || '你好，我是content-script呀，我主动发消息给后台！' }, function (response) {
+    tip('收到来自后台的回复：' + response);
+  });
 }
--- Z-NsertV0.3/popup.js
+++ Z-NsertV0.3/popup.js
@@ -1,35 +1,35 @@
 ﻿console.log('zhl test');
 
-function init() {
-    const content = $('#content');
-    const current = $('#current');
-    content.empty();
-    current.empty();
+function init () {
+  const content = $('#content');
+  const current = $('#current');
+  content.empty();
+  current.empty();
 }
 //test
 $('#get_audio_upload').click(e => {
-    console.log(document);
-    // 要给content-srcipt 发送消息获得当前页面的dom
-    sendMessageToContentScript({ cmd: 'get_audio_upload' }, function (response) {
-        console.log('1111', response);
-        const audios = JSON.parse(response);
-        $('#import_code_block').css("display", 'none');
-        const content = $('#content');
-        const current = $('#current');
-        init();
-        current.append($(`
+  console.log(document);
+  // 要给content-srcipt 发送消息获得当前页面的dom
+  sendMessageToContentScript({ cmd: 'get_audio_upload' }, function (response) {
+    console.log('1111', response);
+    const audios = JSON.parse(response);
+    $('#import_code_block').css("display", 'none');
+    const content = $('#content');
+    const current = $('#current');
+    init();
+    current.append($(`
             <div>提取音频(用户上传):</div>
 
         `))
-        for (let i in audios) {
-            const audio = audios[i];
-            const item = $(`<div>
+    for (let i in audios) {
+      const audio = audios[i];
+      const item = $(`<div>
             <div>名称:<span style="color:orangered">${audio.name}</span> | 请复制完整下方代码↓</div>
             <input style="margin-top:4px;display:inline-block;width:235px;outline:none;font-size:10px" value="${audio.code}" /> 
             </div>`)
-            content.append(item)
-        }
-    });
+      content.append(item)
+    }
+  });
 });
 
 
@@ -41,139 +41,144 @@ $('#get_audio_upload').click(e => {
 
 
 $('#get_audio_qqmusic').click(e => {
-    console.log(document);
-    // 要给content-srcipt 发送消息获得当前页面的dom
-    sendMessageToContentScript({ cmd: 'get_audio_qqmusic' }, function (response) {
-        console.log(response);
-        const audios = JSON.parse(response);
-        $('#import_code_block').css("display", 'none');
-        const content = $('#content');
-        const current = $('#current');
-        init();
-        current.append($(`
+  console.log(document);
+  // 要给content-srcipt 发送消息获得当前页面的dom
+  sendMessageToContentScript({ cmd: 'get_audio_qqmusic' }, function (response) {
+    console.log(response);
+    const audios = JSON.parse(response);
+    $('#import_code_block').css("display", 'none');
+    const content = $('#content');
+    const current = $('#current');
+    init();
+    current.append($(`
             <div>提取音频(QQ音乐):</div>
 
         `))
 
-        for (let i in audios) {
-            const audio = audios[i];
-            const item = $(`<div>
+    for (let i in audios) {
+      const audio = audios[i];
+      const item = $(`<div>
             <div>名称:<span style="color:orangered">${audio.name}</span> | 请复制完整下方代码↓</div>
             <textarea style="margin-top:4px;display:inline-block;width:235px;outline:none;font-size:10px;height:40px;">${audio.code}</textarea>
             </div>`)
-            content.append(item)
-        }
+      content.append(item)
+    }
 
-    });
+  });
 });
 
 
 $('#get_video_upload').click(e => {
-    console.log(document);
-    // 要给content-srcipt 发送消息获得当前页面的dom
-    sendMessageToContentScript({ cmd: 'get_video_upload' }, function (response) {
-        console.log(response);
-        const videos = JSON.parse(response);
-        $('#import_code_block').css("display", 'none');
-        const content = $('#content');
-        const current = $('#current');
-        init();
-        current.append($(`
+  console.log(document);
+  // 要给content-srcipt 发送消息获得当前页面的dom
+  sendMessageToContentScript({ cmd: 'get_video_upload' }, function (response) {
+    console.log(response);
+    const videos = JSON.parse(response);
+    $('#import_code_block').css("display", 'none');
+    const content = $('#content');
+    const current = $('#current');
+    init();
+    current.append($(`
             <div>提取视频(用户上传):</div>
         `))
-        for (let i in videos) {
-            const video = videos[i];
-            const c = parseInt(i + 1);
-            const item = $(`<div>
+    for (let i in videos) {
+      const video = videos[i];
+      const c = parseInt(i + 1);
+      const item = $(`<div>
             <div>编号:${c},请复制完整下方代码↓</div>
             <input style="margin-top:4px;display:inline-block;width:235px;outline:none;font-size:10px;" value="${video.code}">
             </div>`)
-            content.append(item)
-        }
+      content.append(item)
+    }
 
-    });
+  });
 });
 
 
 $('#get_mimi_id').click(e => {
-    console.log(document);
-    // 要给content-srcipt 发送消息获得当前页面的dom
-    sendMessageToContentScript({ cmd: 'get_mimi_id' }, function (response) {
-        console.log(response);
-        const minis = JSON.parse(response);
-        $('#import_code_block').css("display", 'none');
-        const content = $('#content');
-        const current = $('#current');
-        init();
-        current.append($(`
+  console.log(document);
+  // 要给content-srcipt 发送消息获得当前页面的dom
+  sendMessageToContentScript({ cmd: 'get_mimi_id' }, function (response) {
+    console.log(response);
+    const minis = JSON.parse(response);
+    $('#import_code_block').css("display", 'none');
+    const content = $('#content');
+    const current = $('#current');
+    init();
+    current.append($(`
             <div>提取小程序(文字形式):</div>
         `))
 
-        for (let i in minis) {
-            const mini = minis[i];
+    for (let i in minis) {
+      const mini = minis[i];
 
 
-            const item = $(`<div>
+      const item = $(`<div>
             <div>名称:<span style="color:orangered">${mini.name}</span> | 请复制完整下方代码↓</div>
             appid:
             <input style="margin-top:4px;display:inline-block;width:235px;outline:none;font-size:10px" value="${mini.appid}" /> 
             path:<input style="margin-top:4px;display:inline-block;width:235px;outline:none;font-size:10px" value="${mini.path}" /> 
             </div>`)
-            content.append(item)
+      content.append(item)
 
-        }
-    });
+    }
+  });
 });
 
 $('#show_code_textarea').click(e => {
-    const content = $('#content');
-    init();
+  const content = $('#content');
+  init();
 
-    $('#import_code_block').css("display", 'block');
+  $('#import_code_block').css("display", 'block');
 })
 $('#import_code').click(e => {
-    // console.log(document);
-    // 要给content-srcipt 发送消息获得当前页面的dom
-    const value = $('#import_code_area').val();
-    console.log(value);
-    const content = $('#content');
-    content.empty();
+  // console.log(document);
+  // 要给content-srcipt 发送消息获得当前页面的dom
+  const value = $('#import_code_area').val();
+  console.log(value);
+  const content = $('#content');
+  content.empty();
 
-    if (!value) return alert("请输入代码！");
-    sendMessageToContentScript({ cmd: 'import_code', code: value }, function (response) {
-        // $('import_code_block').css("display", 'none');
-        alert("导入成功")
-    });
+  // if (!value) return alert("请输入代码！");
+  sendMessageToContentScript({ cmd: 'import_code', code: value }, function (response) {
+    // $('import_code_block').css("display", 'none');
+    // alert("导入成功")
+    // chrome.notifications.create(null, {
+    //   type: 'basic',
+    //   title: '温馨提示',
+    //   message: '导入成功'
+    // });
+  });
 });
 $('#clear_code').click(e => {
-    // console.log(document);
-    // 要给content-srcipt 发送消息获得当前页面的dom
-    init();
+  // console.log(document);
+  // 要给content-srcipt 发送消息获得当前页面的dom
+  init();
 
-    const value = '';
-    sendMessageToContentScript({ cmd: 'import_code', code: value }, function (response) {
-        // $('import_code_block').css("display", 'none');
-        alert("清空成功")
-    });
+  const value = '';
+  sendMessageToContentScript({ cmd: 'import_code', code: value }, function (response) {
+    // $('import_code_block').css("display", 'none');
+    // alert("清空成功")
+  });
 });
 // $('#update_font_size').click(() => {
 //     sendMessageToContentScript({ cmd: 'update_font_size', size: 42 }, function (response) { });
 // });
 
 // 获取当前选项卡ID
-function getCurrentTabId(callback) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (callback) callback(tabs.length ? tabs[0].id : null);
-    });
+function getCurrentTabId (callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (callback) callback(tabs.length ? tabs[0].id : null);
+  });
 }
 
 // 向content-script主动发送消息
-function sendMessageToContentScript(message, callback) {
-    getCurrentTabId((tabId) => {
-        chrome.tabs.sendMessage(tabId, message, function (response) {
-            if (callback) callback(response);
-        });
+function sendMessageToContentScript (message, callback) {
+  getCurrentTabId((tabId) => {
+    chrome.tabs.sendMessage(tabId, message, function (response) {
+      if (callback) callback(response);
     });
+  });
 }
 
 
